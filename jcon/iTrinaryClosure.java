9e161328a3b9b2af58aadd21a6bb2949120f2d39
todd <>
1998-03-02 21:29:53 +0000

    Treat 3-arity ops like 1 and 2-.

---
+++ jcon/iTrinaryClosure.java
@@ -0,0 +1,34 @@
+package rts;
+
+public abstract class iTrinaryClosure extends iClosure {
+
+	vDescriptor argument0, argument1, argument2;
+
+	abstract vDescriptor function();
+
+	abstract public vDescriptor call(vDescriptor arg0, vDescriptor arg1, vDescriptor arg2, iClosure parent); 
+
+	public void closure(vDescriptor[] args, iClosure parent) {
+		init();
+		argument0 = args[0];
+		argument1 = args[1];
+		argument2 = args[2];
+		this.parent = parent;
+	}
+	public void closure(vDescriptor arg0,vDescriptor arg1, vDescriptor arg2, iClosure parent) {
+		init();
+		argument0 = arg0;
+		argument1 = arg1;
+		argument2 = arg2;
+		this.parent = parent;
+	}
+
+	String getarg(int n) {
+		switch (n) {
+		default: return "???";
+		case 0 : return argument0.report();
+		case 1 : return argument1.report();
+		case 2 : return argument2.report();
+		}
+	}
+}
---
+++ jcon/iTrinaryRefClosure.java
@@ -0,0 +1,35 @@
+package rts;
+
+public abstract class iTrinaryRefClosure extends iTrinaryClosure {
+
+	// Class for simple trinary operations that return at most one value
+	// but do not want their arguments dereferenced
+
+	public vDescriptor nextval() {
+		if (PC == 1) {
+			vDescriptor v = function();
+			PC = 0;
+			return v;
+		} else {
+			return null;
+		}
+	}
+
+	public vDescriptor call(vDescriptor arg0, vDescriptor arg1, vDescriptor arg2, iClosure parent) {
+	    argument0 = arg0;
+	    argument1 = arg1;
+	    argument2 = arg2;
+	    this.parent = parent;
+	    try {
+		try {
+		    return function();
+		} catch (OutOfMemoryError e) {
+		    iRuntime.error(307);	// #%#%# really out of memory.
+		    return null;
+		}
+	    } catch (iError e) {
+		e.report(this);  // returns only on error->failure conversion.
+		return null;
+	    }
+	}
+}
