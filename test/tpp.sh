f37106c6850fe061e4f7b605f122e2d5d42711f3
gmt <>
1998-04-30 17:22:36 +0000

    Preprocessor test was not running because file name was not ".std".

--- test/tpp.sh
+++ test/tpp.sh
@@ -2,7 +2,7 @@
 #
 #  special test of preprocessing and related error detection
 
-../bin/jtran -E tpp.icn >tpp.out 2>tpp.err
+../bin/jcont -E tpp.icn >tpp.out 2>tpp.err
 set -e
-cmp tpp.stdo tpp.out
+cmp tpp.std tpp.out
 cmp tpp.stde tpp.err
