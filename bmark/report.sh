dde9ff0e8283e92cadf68f01d8e2d2b3424ab78a
gmt <>
1998-05-11 18:00:11 +0000

    Make script more portable.

--- bmark/report.sh
+++ bmark/report.sh
@@ -6,14 +6,12 @@ echo ''
 echo '          icont  jcont    ratio'
 for p; do
     cat $p.it $p.jt | sed '
-	# handle Linux by transforming to true Korn Shell format
 	s/.*real *\(.*\)user.*system/user 0m\1/
-	# edit out 'm' and 's' annotations
 	s/m/ /
 	s/s *$//
     ' | awk '
        /user/	{ t1 = t2; t2 = 60 * $2 + $3; }
-       END      { printf("%-8s %6.2f %6.2f %8.2f\n",
+       END      { printf("%-8s %6.2f %6.2f %8.2f\n", \
        		  "'$p'", t1, t2, t2 / t1); }
     '
 done
