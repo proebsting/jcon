8a70445924528795061a2cd5d31dd1caebc059df
todd <>
1998-06-04 23:29:01 +0000

    Crude tracing.

--- jcon/iError.java
+++ jcon/iError.java
@@ -6,6 +6,9 @@ public class iError extends Error {
 
     int num;				// error number
     vDescriptor desc;			// offending value
+    String message = "";		// propagated message
+    String filename;
+    int lineno;
 
 
 
@@ -18,34 +21,43 @@ iError(int num, vDescriptor desc) {	// constructor
 
 void report() {				// print message and abort
 
+
+    k$output.file.flush();
+    vFile f = k$errout.file;
+    f.newline();
+    f.println("Run-time error " + num);
+    if (filename != null) {
+        f.println("File " + filename + "; Line " + lineno);
+    }
+    f.println(iRunerr.text(num));
+    if (desc != null) {
+	f.println("offending value: " + desc.report());
+    }
+    f.println("Traceback:");
+    f.println(message);
+
+    this.printStackTrace(); 	//#%#% temporary
+
+    iRuntime.exit(1);
+}
+
+public vDescriptor propagate(String fname, int lineno) {
     // if &error is zero, issue message and abort
     // if &error is not zero, decrement it and set other error keywords
     if (k$error.self.check()) {
 	k$errornumber.self.set(vInteger.New(this.num));
 	k$errortext.self.set(vString.New(iRunerr.text(num)));
 	k$errorvalue.self.set((this.desc == null) ? null : this.desc.Deref());
-//#%#%#% error conversion is not functional right now
-	System.err.println("\t\terror conversion not implemented");
-	iRuntime.exit(1);
-//#%#%#%
-	return;
+        return null;
+    } else {
+	if (filename == null) {
+	    this.lineno = lineno;
+	    this.filename = fname;
+	}
+	message = "   line " + lineno + " in " + fname + "\n" + message;
+        throw this;
     }
-
-    k$output.file.flush();
-    vFile f = k$errout.file;
-    f.newline();
-    f.println("Run-time error " + num);
-    //#%#% here is where we would give file/line info
-    f.println(iRunerr.text(num));
-    if (desc != null) {
-	f.println("offending value: " + desc.report());
-    }
-    //#%#% here is where we would do traceback
-    this.printStackTrace(); 	//#%#% temporary
-
-    iRuntime.exit(1);
 }
-
 
 
 } // class iError
