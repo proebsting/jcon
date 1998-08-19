103b64e82f092b8981aa7757941a30715cc4c887
gmt <>
1998-08-19 19:41:13 +0000

    Enhance loadfunc test re loading of Icon procedures.

--- test/loadfunc.sh
+++ test/loadfunc.sh
@@ -5,7 +5,8 @@
 set -e
 CLASSPATH=../bin/rts.zip javac factors.java sum3.java threen.java
 jar cfM jfuncs.zip factors*.class sum3*.class threen*.class
+$JCONT -c load[12].icn
 $JCONT -s loadfunc.icn
 ./loadfunc >loadfunc.out
 cmp loadfunc.std loadfunc.out
-rm jfuncs.zip factors*.class sum3*.class threen*.class
+rm jfuncs.zip load?.zip factors*.class sum3*.class threen*.class
