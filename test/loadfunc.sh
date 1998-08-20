05eb4e04e60676210c5a153c7c15d479a07d08d2
gmt <>
1998-08-20 00:44:41 +0000

    Compile silently.

--- test/loadfunc.sh
+++ test/loadfunc.sh
@@ -5,7 +5,7 @@
 set -e
 CLASSPATH=../bin/rts.zip javac factors.java sum3.java threen.java
 jar cfM jfuncs.zip factors*.class sum3*.class threen*.class
-$JCONT -c load[12].icn
+$JCONT -s -c load[12].icn
 $JCONT -s loadfunc.icn
 ./loadfunc >loadfunc.out
 cmp loadfunc.std loadfunc.out
