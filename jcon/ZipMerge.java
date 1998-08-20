2477d927c2673a6aca5b1615ce2a9305242fa5b7
gmt <>
1998-08-20 20:49:17 +0000

    In ZipMerge, handle recompression to different size, and add -v.

--- jcon/ZipMerge.java
+++ jcon/ZipMerge.java
@@ -21,15 +21,24 @@ public class ZipMerge {
 
 
 
+static boolean verbose = false;
+
+
+
 public static void main(String[] args) {
-    if (args.length == 0) {
-	System.err.println("Usage: ZipMerge destination sourcefiles....");
+    int i = 0;
+    if (args.length > 0 && args[0].equals("-v")) {
+	verbose = true;
+	i++;
+    }
+    if (i >= args.length) {
+	System.err.println("Usage: ZipMerge [-v] destination sourcefiles....");
 	System.exit(1);
     }
-    String dst = args[0];
+    String dst = args[i++];
     Vector v = new Vector();
-    for (int i = 1; i < args.length; i++) {
-	v.addElement(args[i]);
+    while (i < args.length) {
+	v.addElement(args[i++]);
     }
     Enumeration enum = v.elements();
     compose(dst, enum);
@@ -41,8 +50,20 @@ static byte[] buffer = new byte[10000];
 
 public static void addZipEntry(ZipEntry ze, ZipOutputStream zos,
 				InputStream is) throws IOException {
+
+    // need a new ZipEntry that echoes the input one
+    // but does not have the "compressedSize" field set
+    // (it may recompress to a slightly different size)
+    ZipEntry ne = new ZipEntry(ze.getName());
+    if (ze.getTime() != -1) 	{ ne.setTime(ze.getTime()); }
+    if (ze.getSize() != -1) 	{ ne.setSize(ze.getSize()); }
+    if (ze.getCrc() != -1) 	{ ne.setCrc(ze.getCrc()); }
+    if (ze.getMethod() != -1) 	{ ne.setMethod(ze.getMethod()); }
+    if (ze.getExtra() != null) 	{ ne.setExtra(ze.getExtra()); }
+    if (ze.getComment()!=null)	{ ne.setComment(ze.getComment()); }
+
     try {
-	zos.putNextEntry(ze);
+	zos.putNextEntry(ne);
     } catch (ZipException e) {
 	return;	// duplicate entry, ignored.
     }
@@ -82,6 +103,9 @@ public static void compose(String dst, Enumeration files) {
 	    } catch (Exception e) {
 		// (most Javas throw ZipException, but 1.2b4 throws IOException)
 		// handle regular file, not zip.
+		if (verbose) {
+		    System.err.println("   plainfile: " + fname);
+		}
 		FileInputStream fis = new FileInputStream(fname);
 		BufferedInputStream bis = new BufferedInputStream(fis);
 		ZipEntry zf = new ZipEntry(fname);
@@ -89,9 +113,15 @@ public static void compose(String dst, Enumeration files) {
 		bis.close();
 		continue;
 	    }
+	    if (verbose) {
+		System.err.println("   zipfile:   " + fname);
+	    }
 	    Enumeration e = zsrc.entries();
 	    while (e.hasMoreElements()) {
 		ZipEntry ze = (ZipEntry) e.nextElement();
+		if (verbose) {
+		    System.err.println("      " + ze.getName());
+		}
 		InputStream zis = zsrc.getInputStream(ze);
 		addZipEntry(ze, zos, zis);
 		zis.close();
@@ -99,7 +129,11 @@ public static void compose(String dst, Enumeration files) {
 	}
 	zos.close();
     } catch (IOException ex) {
-	System.err.println(ex);
+	if (verbose) {
+	    ex.printStackTrace();
+	} else {
+	    System.err.println(ex);
+	}
 	System.exit(1);
     }
 }
