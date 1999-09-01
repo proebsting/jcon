e1be0297225df0f777aed8387022e793f4964ec7
gmt <>
1999-09-01 20:08:12 +0000

    Update version number and date to Sept 7th.

--- jcon/iConfig.java
+++ jcon/iConfig.java
@@ -8,7 +8,7 @@ final class iConfig {
 
 // the following constants are peculiar to the Jcon implementation
 
-static final String Version = "Jcon Version 2.1.c, May 21, 1999";
+static final String Version = "Jcon Version 2.1, September 7, 1999";
 
 static final String PackageName = "jcon";	// class prefix
 static final String KeywordPrefix = "k$";	// name prefix for keywords
